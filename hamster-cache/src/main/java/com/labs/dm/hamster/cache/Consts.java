9fbc99bb3baf54b5005a4cb095e6dbb301dbe2b6
daniel <daniel.mroczka@gmail.com>
2015-02-22 08:26:54 +0100

    added file

---
+++ hamster-cache/src/main/java/com/labs/dm/hamster/cache/Consts.java
@@ -0,0 +1,9 @@
+package com.labs.dm.hamster.cache;
+
+/**
+ * @author daniel
+ */
+public class Consts {
+
+    public static final String CONF_KEY = "com.labs.dm.hamster.configurationResourceName";
+}
